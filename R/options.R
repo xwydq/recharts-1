bd8c7d24be4e115eef636b98f48749a551c2b090
madlogos <madlogos@gmail.com>
2017-06-14 23:11:18 +0800

    fix tooltip formatter

--- R/options.R
+++ R/options.R
@@ -2651,7 +2651,8 @@ setTooltip <- function(chart, series=NULL, timeslots=NULL, trigger=NULL,
 
     eval(parse(text=paste(lhs, "<-", rhs)))
     if (class(formatter) %in% c("JS_EVAL", "character"))
-        chart$x$tooltip$formatter <- formatter
+        if (formatter != 'none') chart$x$tooltip$formatter <- formatter
+
     return(chart)
 }
 
